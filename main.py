91d73eab7c2f2c13af019c0eeb7b8f510e8ac23e
Guru <guruarunachalam17@gmail.com>
2026-07-16 00:20:15 +0530

    Performed dataset exploration using Pandas

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Air Quality Prediction Project")
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv("data/raw/AirQualityUCI.csv", sep=";")
+
+print("========== AIR QUALITY PREDICTION PROJECT ==========\n")
+
+# First 5 rows
+print("First 5 Rows:")
+print(df.head())
+
+# Shape
+print("\nDataset Shape:")
+print(df.shape)
+
+# Column names
+print("\nColumns:")
+print(df.columns)
+
+# Information
+print("\nDataset Information:")
+df.info()
+
+# Missing values
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+# Statistical Summary
+print("\nStatistical Summary:")
+print(df.describe())
