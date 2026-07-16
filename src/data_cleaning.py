c0ce9fd90c8e29751fac4f3229c06a9a700773ea
Guru <guruarunachalam17@gmail.com>
2026-07-16 21:39:34 +0530

    Added data cleaning and missing value analysis

---
+++ src/data_cleaning.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("data/raw/AirQualityUCI.csv", sep=";")
+
+# Remove unwanted columns
+df = df.drop(columns=["Unnamed: 15", "Unnamed: 16"])
+
+# Replace invalid values (-200) with NaN
+df.replace(-200, pd.NA, inplace=True)
+
+print("Shape after cleaning:", df.shape)
+print(df.shape)
+
+print("\nMissing values:")
+print(df.isnull().sum())
